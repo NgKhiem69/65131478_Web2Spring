af7dfe81fe529c91f9b6060009505841bc195a20
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-03-11 01:16:19 -0700

    hehe
    hehe

--- NguyenGiaKhiem_fitCMS/src/main/java/thiGK/ntu65131478/HomeController.java
+++ NguyenGiaKhiem_fitCMS/src/main/java/thiGK/ntu65131478/HomeController.java
@@ -1,5 +1,21 @@
 package thiGK.ntu65131478;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+	@Controller
+	public class HomeController {
+
+	    @GetMapping("/")
+	    public String index(Model model) {
+	        model.addAttribute("username", "Student Name");
+	        return "index";
+	    }
+
+	    @GetMapping("/dashboard")
+	    public String dashboard(Model model) {
+	        model.addAttribute("username", "Student Name");
+	        return "index";
+	    }
 }
