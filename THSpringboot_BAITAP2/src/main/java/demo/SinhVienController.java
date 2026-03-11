e8243c3e911e72bcec65238ef22a89e3836401f6
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-03-11 03:52:50 -0700

    add sv2
    add sv2

---
+++ THSpringboot_BAITAP2/src/main/java/demo/SinhVienController.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class SinhVienController {
+	@GetMapping("/sinhvien")
+	public String sinhVienInfo(Model model) {
+        model.addAttribute("mssv", "65134116");
+        model.addAttribute("hoten", "Nguyễn Huỳnh Tường");
+        model.addAttribute("namsinh", "2005");
+        model.addAttribute("gioitinh", "Nam");
+        
+        return "sinhVienView";
+	}
+}
---
+++ THSpringboot_BAITAP2/src/main/java/thiGK/ntu65131478/ThSpringbootBaitap2Application.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65131478;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThSpringbootBaitap2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThSpringbootBaitap2Application.class, args);
+	}
+
+}
