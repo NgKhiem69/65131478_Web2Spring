b4e97aa715444a1e96beac07fa0a457e8c3168de
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-03-11 00:49:37 -0700

    gk2
    thigk

---
+++ NguyenGiaKhiem_fitCMS/src/main/java/thiGK/ntu65131478/NguyenGiaKhiemFitCmsApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65131478;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenGiaKhiemFitCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenGiaKhiemFitCmsApplication.class, args);
+	}
+
+}
---
+++ NguyenGiaKhiem_fitCMS/src/test/java/thiGK/ntu65131478/NguyenGiaKhiemFitCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65131478;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenGiaKhiemFitCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
