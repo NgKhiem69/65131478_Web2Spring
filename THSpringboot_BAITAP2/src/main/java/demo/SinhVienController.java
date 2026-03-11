68d272e88314aa25e5655e3f116f2b913e7dc99a
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-03-11 03:55:06 -0700

    btap2

--- THSpringboot_BAITAP2/src/main/java/demo/SinhVienController.java
+++ THSpringboot_BAITAP2/src/main/java/demo/SinhVienController.java
@@ -8,8 +8,8 @@ import org.springframework.ui.Model;
 public class SinhVienController {
 	@GetMapping("/sinhvien")
 	public String sinhVienInfo(Model model) {
-        model.addAttribute("mssv", "65134116");
-        model.addAttribute("hoten", "Nguyễn Huỳnh Tường");
+        model.addAttribute("mssv", "65131478");
+        model.addAttribute("hoten", "Nguyễn Gia Khiêm");
         model.addAttribute("namsinh", "2005");
         model.addAttribute("gioitinh", "Nam");
         
