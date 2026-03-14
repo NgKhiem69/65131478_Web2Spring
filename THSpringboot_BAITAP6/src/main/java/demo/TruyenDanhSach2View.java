0240cd00022fcf29fdab8e35c36304d1ef7785b1
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-03-14 16:02:27 +0700

    baitap6

---
+++ THSpringboot_BAITAP6/src/main/java/demo/TruyenDanhSach2View.java
@@ -0,0 +1,30 @@
+package demo;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import models.SanPham;
+
+@Controller
+public class TruyenDanhSach2View {
+
+	@GetMapping("/vd6")
+	public String hienThiDanhSachSanPham(Model model) {
+		List<SanPham> listSP = new ArrayList<>();
+
+		listSP.add(new SanPham("SP001", "Laptop Dell XPS 15", 25000000));
+		listSP.add(new SanPham("SP002", "Chuột không dây Logitech", 1200000));
+		listSP.add(new SanPham("SP003", "Bàn phím cơ Keychron", 1800000));
+		listSP.add(new SanPham("SP004", "Màn hình LG 27 inch 4K", 8500000));
+		listSP.add(new SanPham("SP005", "Tai nghe Sony WH-1000XM5", 7000000));
+
+		model.addAttribute("dsSanPham", listSP);
+
+		return "vd6-danhsach-sanpham";
+	}
+}
