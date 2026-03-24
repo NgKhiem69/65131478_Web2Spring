c2dd841ccdd8e78be12210ef02f85ea2c5f38fce
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-03-24 22:32:51 +0700

    add vidu mySQL

---
+++ mySQL_pagepost/src/main/java/demo/MySqlPagepostApplication.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlPagepostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySqlPagepostApplication.class, args);
+	}
+
+}
