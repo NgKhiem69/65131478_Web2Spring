298f0e15327902b79d7eb3c1c9ae5ffd9ea9701b
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-03-24 22:44:54 +0700

    Update PageRepository.java

--- mySQL_pagepost/src/main/java/repository/PageRepository.java
+++ mySQL_pagepost/src/main/java/repository/PageRepository.java
@@ -1,5 +1,7 @@
 package repository;
 
-public class PageRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import model.Page;
 
+public interface PageRepository extends JpaRepository<Page, Long> {
 }
