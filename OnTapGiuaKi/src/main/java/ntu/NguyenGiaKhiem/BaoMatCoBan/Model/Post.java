d69ec921a2c9e98b428680e9e48553ae7f41bb6d
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-04-15 02:01:23 +0700

    Update Post.java

--- OnTapGiuaKi/src/main/java/ntu/NguyenGiaKhiem/BaoMatCoBan/Model/Post.java
+++ OnTapGiuaKi/src/main/java/ntu/NguyenGiaKhiem/BaoMatCoBan/Model/Post.java
@@ -1,5 +1,53 @@
 package ntu.NguyenGiaKhiem.BaoMatCoBan.Model;
 
-public class Post {
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String title;
+    
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    
+    private Long categoryId;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+    
 }
