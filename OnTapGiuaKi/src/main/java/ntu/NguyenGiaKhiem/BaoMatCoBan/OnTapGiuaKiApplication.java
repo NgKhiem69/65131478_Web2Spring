536fba77ab96c6f9aaa1edc21a66e583b01312b1
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-04-15 01:30:44 +0700

    add ontap

---
+++ OnTapGiuaKi/src/main/java/ntu/NguyenGiaKhiem/BaoMatCoBan/OnTapGiuaKiApplication.java
@@ -0,0 +1,13 @@
+package ntu.NguyenGiaKhiem.BaoMatCoBan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnTapGiuaKiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnTapGiuaKiApplication.class, args);
+	}
+
+}
---
+++ OnTapGiuaKi/src/test/java/ntu/NguyenGiaKhiem/BaoMatCoBan/OnTapGiuaKiApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.NguyenGiaKhiem.BaoMatCoBan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnTapGiuaKiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
