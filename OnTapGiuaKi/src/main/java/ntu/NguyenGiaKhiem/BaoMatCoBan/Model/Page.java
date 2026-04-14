226624f770d3c038b3ee42e13c23e3f18ef9b370
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-04-15 02:01:55 +0700

    Update Page.java

--- OnTapGiuaKi/src/main/java/ntu/NguyenGiaKhiem/BaoMatCoBan/Model/Page.java
+++ OnTapGiuaKi/src/main/java/ntu/NguyenGiaKhiem/BaoMatCoBan/Model/Page.java
@@ -1,5 +1,61 @@
 package ntu.NguyenGiaKhiem.BaoMatCoBan.Model;
 
-public class Page {
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "pages")
+public class Page {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String pageName;
+    private String keyword;
+    
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    
+    private Long parentPageId;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPageName() {
+		return pageName;
+	}
+
+	public void setPageName(String pageName) {
+		this.pageName = pageName;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Long getParentPageId() {
+		return parentPageId;
+	}
+
+	public void setParentPageId(Long parentPageId) {
+		this.parentPageId = parentPageId;
+	}
+    
 }
