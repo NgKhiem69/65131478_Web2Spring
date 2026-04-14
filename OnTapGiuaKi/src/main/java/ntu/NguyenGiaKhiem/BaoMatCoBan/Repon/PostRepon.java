10b1bddfaf3288b287ed4b0ae08f134185fdaace
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-04-15 02:00:49 +0700

    Update PostRepon.java

--- OnTapGiuaKi/src/main/java/ntu/NguyenGiaKhiem/BaoMatCoBan/Repon/PostRepon.java
+++ OnTapGiuaKi/src/main/java/ntu/NguyenGiaKhiem/BaoMatCoBan/Repon/PostRepon.java
@@ -1,5 +1,8 @@
 package ntu.NguyenGiaKhiem.BaoMatCoBan.Repon;
 
-public class PostRepon {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import ntu.NguyenGiaKhiem.BaoMatCoBan.Model.Post;
+
+public interface PostRepon extends JpaRepository<Post, Long> {
 }
