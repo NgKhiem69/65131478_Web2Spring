caef23ee4ddae5e23845803bf4ad8551363f3f19
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-04-22 09:29:41 +0700

    update

--- ChuanBiThiGK2/src/main/java/thigk2/NguyenGiaKhiem/reponsitory/LoaiTinTucRepository.java
+++ ChuanBiThiGK2/src/main/java/thigk2/NguyenGiaKhiem/reponsitory/LoaiTinTucRepository.java
@@ -1,5 +1,7 @@
 package thigk2.NguyenGiaKhiem.reponsitory;
 
-public class LoaiTinTucRepository {
+import thigk2.NguyenGiaKhiem.model.LoaiTinTuc;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface LoaiTinTucRepository extends JpaRepository<LoaiTinTuc, Integer> {
 }
