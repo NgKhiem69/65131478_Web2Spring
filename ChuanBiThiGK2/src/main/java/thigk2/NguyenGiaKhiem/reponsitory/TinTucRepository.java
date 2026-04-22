2af545be1b54fa680a0cd5263855d7734cc42c06
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-04-22 08:59:14 +0700

    Update TinTucRepository.java

--- ChuanBiThiGK2/src/main/java/thigk2/NguyenGiaKhiem/reponsitory/TinTucRepository.java
+++ ChuanBiThiGK2/src/main/java/thigk2/NguyenGiaKhiem/reponsitory/TinTucRepository.java
@@ -1,5 +1,7 @@
 package thigk2.NguyenGiaKhiem.reponsitory;
 
-public class TinTucRepository {
+import thigk2.NguyenGiaKhiem.model.TinTuc;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface TinTucRepository extends JpaRepository<TinTuc, Integer> {
 }
