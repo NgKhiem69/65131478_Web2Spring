f11751b603f0acf9540d4a165692286b99f5de72
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-04-22 09:12:54 +0700

    Update HomeController.java

--- ChuanBiThiGK2/src/main/java/thigk2/NguyenGiaKhiem/controller/HomeController.java
+++ ChuanBiThiGK2/src/main/java/thigk2/NguyenGiaKhiem/controller/HomeController.java
@@ -1,5 +1,21 @@
 package thigk2.NguyenGiaKhiem.controller;
 
+import thigk2.NguyenGiaKhiem.service.TinTucService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
+    @Autowired
+    private TinTucService tinTucService;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        // Lấy danh sách tin tức hiển thị ra trang chủ
+        model.addAttribute("tinTucMoiNhat", tinTucService.getAllTinTuc());
+        return "index"; // Trả về file src/main/resources/templates/index.html
+    }
 }
