5ebb0a564812c3e8449b7315d8123bd940347a95
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-04-22 08:56:59 +0700

    Update TinTuc.java

--- ChuanBiThiGK2/src/main/java/thigk2/NguyenGiaKhiem/model/TinTuc.java
+++ ChuanBiThiGK2/src/main/java/thigk2/NguyenGiaKhiem/model/TinTuc.java
@@ -1,5 +1,22 @@
 package thigk2.NguyenGiaKhiem.model;
 
-public class TinTuc {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "tin_tuc")
+public class TinTuc {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    private String tieuDe;
+    
+    @Column(columnDefinition = "TEXT")
+    private String noiDung;
+
+    @ManyToOne
+    @JoinColumn(name = "loai_tin_tuc_id")
+    private LoaiTinTuc loaiTinTuc;
+
+    // Getters và Setters
 }
