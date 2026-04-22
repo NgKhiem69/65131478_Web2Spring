1be3ede2b8f21e51194756c3c56f0365ea6e8a53
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-04-22 09:02:53 +0700

    Update TinTucService.java

--- ChuanBiThiGK2/src/main/java/thigk2/NguyenGiaKhiem/service/TinTucService.java
+++ ChuanBiThiGK2/src/main/java/thigk2/NguyenGiaKhiem/service/TinTucService.java
@@ -1,5 +1,18 @@
 package thigk2.NguyenGiaKhiem.service;
 
-public class TinTucService {
+import thigk2.NguyenGiaKhiem.model.TinTuc;
+import thigk2.NguyenGiaKhiem.reponsitory.TinTucRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
+@Service
+public class TinTucService {
+    @Autowired
+    private TinTucRepository repo;
+
+    public List<TinTuc> getAllTinTuc() { return repo.findAll(); }
+    public void save(TinTuc tinTuc) { repo.save(tinTuc); }
+    public TinTuc getById(Integer id) { return repo.findById(id).orElse(null); }
+    public void delete(Integer id) { repo.deleteById(id); }
 }
