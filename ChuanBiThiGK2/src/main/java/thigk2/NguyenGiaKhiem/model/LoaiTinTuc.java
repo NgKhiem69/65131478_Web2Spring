f5d8cd3f106c6aa87ff7d62cb1f9e3d583d454f3
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-04-22 08:56:36 +0700

    Update LoaiTinTuc.java

--- ChuanBiThiGK2/src/main/java/thigk2/NguyenGiaKhiem/model/LoaiTinTuc.java
+++ ChuanBiThiGK2/src/main/java/thigk2/NguyenGiaKhiem/model/LoaiTinTuc.java
@@ -1,5 +1,19 @@
 package thigk2.NguyenGiaKhiem.model;
 
-public class LoaiTinTuc {
+import jakarta.persistence.*;
+import java.util.List;
 
+@Entity
+@Table(name = "loai_tin_tuc")
+public class LoaiTinTuc {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    private String tenLoai;
+
+    @OneToMany(mappedBy = "loaiTinTuc", cascade = CascadeType.ALL)
+    private List<TinTuc> danhSachTinTuc;
+
+    // Getters và Setters (hoặc dùng @Data của Lombok)
 }
