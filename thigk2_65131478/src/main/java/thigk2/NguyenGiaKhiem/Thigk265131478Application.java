11e6302687430082f03d1794970f25f79c9c8528
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-04-24 13:46:04 +0700

    creat baithigk2
    COMMIT LẦN THỨ 1

---
+++ thigk2_65131478/src/main/java/thigk2/NguyenGiaKhiem/Thigk265131478Application.java
@@ -0,0 +1,13 @@
+package thigk2.NguyenGiaKhiem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thigk265131478Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Thigk265131478Application.class, args);
+	}
+
+}
---
+++ thigk2_65131478/src/test/java/thigk2/NguyenGiaKhiem/Thigk265131478ApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk2.NguyenGiaKhiem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thigk265131478ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
